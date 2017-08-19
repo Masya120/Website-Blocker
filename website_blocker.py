1a2a85c39c0d9cf067467e1255a038dc1d19c4d0
Masya120 <maxk893@gmail.com>
2017-08-19 16:26:43 -0700

    Added some comments and changed default working hours

--- website_blocker.py
+++ website_blocker.py
@@ -1,18 +1,18 @@
 import time
 from datetime import datetime as dt
 
-#use hosts_temp to test script on a copy of hosts in the same directory as script
+# Use hosts_temp to test script on a copy of hosts in the same directory as script
 hosts_temp = "hosts"
 
 # Use hosts_path when you want to edit the actual hosts file on a Linux system
 hosts_path = "/etc/hosts"
 redirect = "127.0.0.1"
 
-
+# Update this list with websites that you want blocked during "working hours"
 website_list = ["www.facebook.com", "facebook.com", "youtube.com", "www.youtube.com"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,19) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,20):
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,17):
         with open(hosts_path, 'r+') as file:
             content = file.read()
             for website in website_list:
