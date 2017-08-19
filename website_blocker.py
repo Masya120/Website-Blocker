d21781af654734057a020db35a02fa3e219d36ff
Masya120 <maxk893@gmail.com>
2017-08-19 16:20:30 -0700

    Initial commit

---
+++ website_blocker.py
@@ -0,0 +1,32 @@
+import time
+from datetime import datetime as dt
+
+#use hosts_temp to test script on a copy of hosts in the same directory as script
+hosts_temp = "hosts"
+
+# Use hosts_path when you want to edit the actual hosts file on a Linux system
+hosts_path = "/etc/hosts"
+redirect = "127.0.0.1"
+
+
+website_list = ["www.facebook.com", "facebook.com", "youtube.com", "www.youtube.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,19) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,20):
+        with open(hosts_path, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write("\n" + redirect + " " + website)
+    else:
+        with open(hosts_path, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()            
+    time.sleep(60)
+
